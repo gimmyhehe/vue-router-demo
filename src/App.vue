2973c42c9b5d736f10a1c237d703317c130e9a50
unknown <975402925@qq.com>
2023-08-06 12:33:27 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <router-link to='/home'>home</router-link> |
+    <router-link to='/about'>about</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+</style>
