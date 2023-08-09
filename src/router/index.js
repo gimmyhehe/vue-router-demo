90db8a9c404e8b9aac8f36b19816a4952ae6db33
unknown <975402925@qq.com>
2023-08-10 01:15:05 +0800

    路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,20 @@ import About from '../components/About.vue'
 import Product from '../components/Product.vue'
 import Login from '../components/Login.vue'
 
+const vipGuard = (to, from) => {
+  console.log('auth-guard')
+  const isVip = sessionStorage.getItem('isVip') === 'true'
+  if (!isVip) {
+    alert('不是vip 请充值！！！')
+    return false
+  }
+}
+
 const routes = [
   { path: '/home', component: Home, sensitive: true },
   { path: '/about', component: About },
   { path: '/Login', component: Login },
-  { path: '/product/:id', component: Product, name: 'product', meta: { needVip: true } }
+  { path: '/product/:id', component: Product, name: 'product', beforeEnter: vipGuard }
 ]
 const router = createRouter({
   history: createWebHistory(),
@@ -25,17 +34,6 @@ const loginGuard = (to, from) => {
   }
 }
 
-const authGuard = (to, from) => {
-  console.log('auth-guard')
-  const isVip = sessionStorage.getItem('isVip') === 'true'
-  if (to.meta.needVip && !isVip) {
-    alert('不是vip 请充值！！！')
-    return false
-  }
-}
-
 router.beforeEach(loginGuard)
-
-router.beforeEach(authGuard)
 
 export default router
