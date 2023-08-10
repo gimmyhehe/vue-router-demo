3421d571b06a1439b84fc9509e0d13a11c7c376a
Gimmy <975402925@qq.com>
2023-08-10 03:31:36 -0700

    命名路由

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,10 @@
     <router-link to='/about'>about</router-link>
     | <router-link to='/product/1'>product1</router-link>
     | <router-link to='/product/2'>product2</router-link>
-    <!-- <template v-if="isAddRoute">
-      | <router-link to='/product/1'>product1</router-link>
-      | <router-link to='/product/2'>product2</router-link>  
-    </template> -->
     <div>
       <button @click="addRoute">加载动态路由</button>
+      <button @click="removeRoute">移除路由</button>
+      <button @click="check">检查路由</button>
     </div>
     <router-view></router-view>
   </div>
@@ -19,18 +17,29 @@
 import { useRouter } from 'vue-router'
 import { ref } from 'vue'
 import Product from './components/Product.vue'
-const isAddRoute = ref(false)
+let remove
 const router = useRouter()
 const addRoute = () => {
   setTimeout(() => {
-    router.addRoute(
+    remove = router.addRoute(
       { path: '/product/:id', name: 'product', component: Product, meta: { needVip: true } }
     )
     console.log('动态添加路由成功')
     console.log(router.getRoutes())
   }, 3000)
 }
+const removeRoute = () => {
+  // router.removeRoute('product')
+  remove()
+}
+const check = () => {
+  console.log('all route', router.getRoutes())
+  console.log('has route', router.hasRoute('product'))
+}
 </script>
 
 <style scoped>
+button + button {
+  margin-left: 10px;
+}
 </style>
