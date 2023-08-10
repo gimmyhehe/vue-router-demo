c33bb0e89499a7672bd01d57289ff4cdc88eecb3
Gimmy <975402925@qq.com>
2023-08-09 23:38:06 -0700

    路由原信息

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,23 @@ import NotFound from '../components/NotFound.vue'
 
 
 const nestedRoutes = [
-  { path: 'home', component: Home },
+  { path: 'home', component: Home, meta: { needVip: false, other: {} } },
   { path: 'about', component: About }
 ]
 
 const routes = [
-  { path: '/layout', component: Layout, children: nestedRoutes },
+  { path: '/layout', component: Layout, children: nestedRoutes, meta: { needVip: true, other: { test: 'test' } } },
   { path: '/:matchPath(.*)*', component: NotFound }
 ]
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, from) => {
+  // console.log(to.matched)
+  console.log('to.meta', to.meta)
+  console.log('from.meta', from.meta)
 })
 
 export default router
