ac5ac213bcba503d0e5e5bc51bbeb5708ce382c0
Gimmy <975402925@qq.com>
2023-08-10 00:00:03 -0700

    order

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -3,3 +3,24 @@
     this is product page. product id is {{ $route.params.id }}  query is {{ JSON.stringify($route.query) }} hash is {{ $route.hash }}
   </div>
 </template>
+<script setup>
+import { onBeforeRouteLeave, onBeforeRouteUpdate } from 'vue-router'
+import {  onMounted, onCreated } from 'vue';
+// onBeforeRouteEnter(() => {
+//   console.log('component-onBeforeRouteEnter')
+// })
+onBeforeRouteUpdate(() => {
+  console.log('component-onBeforeRouteUpdate')
+})
+onBeforeRouteLeave(() => {
+  console.log('component-onBeforeRouteLeave')
+})
+
+onCreated(() => {
+  console.log('onCreated')
+})
+onMounted(() => {
+  console.log('onMounted')
+})
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import About from '../components/About.vue'
-import Product from '../components/Product.vue'
 import Login from '../components/Login.vue'
 
 const routes = [
-  { path: '/home', component: Home, sensitive: true },
+  { path: '/home', component: Home, sensitive: true,
+  },
   { path: '/about', component: About },
   { path: '/Login', component: Login },
-  { path: '/product/:id', component: Product, name: 'product', meta: { needVip: true } }
+  { path: '/product/:id', component: () => {
+    console.log('component resolved')
+    return import('../components/Product.vue')
+  }, name: 'product',
+    beforeEnter() {
+      console.log('route-beforeEnter')
+    }
+  }
 ]
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
 
-const loginGuard = (to, from) => {
-  console.log('login-guard', 'to:', to, 'from:', from)
-  // 返回 false 以取消导航
-  const user = sessionStorage.getItem('user')
-  if (!user && to.path !== '/login') {
-    return '/login'
-    // return false
-  }
-}
+router.beforeEach(() => {
+  console.log('global-beforeEach')
+})
 
-const authGuard = (to, from) => {
-  console.log('auth-guard')
-  const isVip = sessionStorage.getItem('isVip') === 'true'
-  if (to.meta.needVip && !isVip) {
-    alert('不是vip 请充值！！！')
-    return false
-  }
-}
+router.beforeResolve(() => {
+  console.log('global-beforeResolve')
+})
 
-router.beforeEach(loginGuard)
-
-router.beforeEach(authGuard)
+router.afterEach(() => {
+  console.log('global-afterEach')
+})
 
 export default router
