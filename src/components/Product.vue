bc2d7f6ed1b208dcf2baebeb80d5488c1698833d
Gimmy <975402925@qq.com>
2023-08-10 03:42:04 -0700

    fdasf

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -3,3 +3,16 @@
     this is product page. product id is {{ $route.params.id }}  query is {{ JSON.stringify($route.query) }} hash is {{ $route.hash }}
   </div>
 </template>
+<script>
+export default {
+  beforeRouteEnter() {
+    console.log('beforeRouteEnter')
+  },
+  beforeRouteUpdate() {
+    console.log('beforeRouterUpdate')
+  },
+  beforeRouteLeave() {
+    console.log('beforeRouterLeave')
+  },
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -15,27 +15,4 @@ const router = createRouter({
   routes
 })
 
-const loginGuard = (to, from) => {
-  console.log('login-guard', 'to:', to, 'from:', from)
-  // 返回 false 以取消导航
-  const user = sessionStorage.getItem('user')
-  if (!user && to.path !== '/login') {
-    return '/login'
-    // return false
-  }
-}
-
-const authGuard = (to, from) => {
-  console.log('auth-guard')
-  const isVip = sessionStorage.getItem('isVip') === 'true'
-  if (to.meta.needVip && !isVip) {
-    alert('不是vip 请充值！！！')
-    return false
-  }
-}
-
-router.beforeEach(loginGuard)
-
-router.beforeEach(authGuard)
-
 export default router
